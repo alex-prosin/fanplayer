d9e6918bfd955ccfede9df13a590984ee8dfd585
Alexander Prosin <alex@stealthmonitoring.com>
2021-12-17 14:15:40 -0500

    fix C# player

--- player-windows/csharpplayer/PlayerForm.cs
+++ player-windows/csharpplayer/PlayerForm.cs
@@ -11,31 +11,31 @@ namespace csharpplayer
 {
     public partial class PlayerForm : Form
     {
-        [DllImport("fanplayer.dll", EntryPoint = "player_open")]
-        static extern IntPtr player_open(string file, IntPtr hwnd, IntPtr configs);
+        [DllImport("fanplayer.dll", EntryPoint = "player_open", CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr player_open([MarshalAs(UnmanagedType.LPStr)] string file, IntPtr hwnd, IntPtr configs);
 
-        [DllImport("fanplayer.dll", EntryPoint = "player_close")]
+        [DllImport("fanplayer.dll", EntryPoint = "player_close", CallingConvention = CallingConvention.Cdecl)]
         static extern void player_close(IntPtr player);
 
-        [DllImport("fanplayer.dll", EntryPoint = "player_play")]
+        [DllImport("fanplayer.dll", EntryPoint = "player_play", CallingConvention = CallingConvention.Cdecl)]
         static extern void player_play(IntPtr player);
 
-        [DllImport("fanplayer.dll", EntryPoint = "player_pause")]
+        [DllImport("fanplayer.dll", EntryPoint = "player_pause", CallingConvention = CallingConvention.Cdecl)]
         static extern void player_pause(IntPtr player);
 
-        [DllImport("fanplayer.dll", EntryPoint = "player_seek")]
+        [DllImport("fanplayer.dll", EntryPoint = "player_seek", CallingConvention = CallingConvention.Cdecl)]
         static extern void player_seek(IntPtr player, long ms);
 
-        [DllImport("fanplayer.dll", EntryPoint = "player_setrect")]
+        [DllImport("fanplayer.dll", EntryPoint = "player_setrect", CallingConvention = CallingConvention.Cdecl)]
         static extern void player_setrect(IntPtr player, int type, int x, int y, int w, int h);
 
-        [DllImport("fanplayer.dll", EntryPoint = "player_snapshot")]
-        static extern int player_snapshot(IntPtr player, string file, int w, int h, int wait);
+        [DllImport("fanplayer.dll", EntryPoint = "player_snapshot", CallingConvention = CallingConvention.Cdecl)]
+        static extern int player_snapshot(IntPtr player, [MarshalAs(UnmanagedType.LPStr)] string file, int w, int h, int wait);
 
-        [DllImport("fanplayer.dll", EntryPoint = "player_setparam")]
+        [DllImport("fanplayer.dll", EntryPoint = "player_setparam", CallingConvention = CallingConvention.Cdecl)]
         static extern void player_setparam(IntPtr player, uint id, IntPtr param);
 
-        [DllImport("fanplayer.dll", EntryPoint = "player_getparam")]
+        [DllImport("fanplayer.dll", EntryPoint = "player_getparam", CallingConvention = CallingConvention.Cdecl)]
         static extern void player_getparam(IntPtr player, uint id, IntPtr param);
 
         private IntPtr mPlayer;
